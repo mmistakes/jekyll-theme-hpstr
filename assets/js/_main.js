bf99c056781f339031ad525ab762e4bcd1ad4772
Michael Rose <est.michael@gmail.com>
2013-08-24 17:13:07 -0400

    Add Grunt build script

---
+++ assets/js/_main.js
@@ -0,0 +1,26 @@
+/*! Plugin options and other jQuery stuff */
+
+// dl-menu options
+$(function() {
+  $( '#dl-menu' ).dlmenu({
+    animationClasses : { classin : 'dl-animate-in', classout : 'dl-animate-out' }
+  });
+});
+
+// FitVids options
+$(function() {
+  $("article").fitVids();
+});
+
+$(".close-menu").click(function () {
+  $(".menu").toggleClass("disabled");
+  $(".links").toggleClass("enabled");
+});
+
+$(".about").click(function () {
+  $("#about").css('display','block');
+});
+
+$(".close-about").click(function () {
+  $("#about").css('display','');
+});
