58aa241ad58e029540b5916a4400725ef8f177d2
Edgar R. Sandi <edgar.r.sandi@gmail.com>
2014-06-09 01:52:14 -0300

    added the js code to reading-time

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -24,3 +24,10 @@ $(".about").click(function () {
 $(".close-about").click(function () {
   $("#about").css('display','');
 });
+
+$(".entry-reading-time-content").text(function (index, value) {
+  var minutes = Math.round(parseFloat(value));
+  var minutes_label=minutes===1?" minute":" minutes";
+  var reading_time=minutes>0?"about "+minutes+" "+minutes_label+" ":"less than 1 minute";
+  return reading_time;
+});
